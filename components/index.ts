256c4c7d5bcee3c6d452e90a7d9e91f32f08b1a3
Blake Neely <blakeneely21@gmail.com>
2025-03-09 12:14:40 -0500

    feat: adds search make autocomplete

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,9 @@
 import Button from './Button'
+import Filter from './Filter'
 import Footer from './Footer'
 import Hero from './Hero'
 import Navbar from './Navbar'
+import Searchbar from './Searchbar'
+import SearchManufacturer from './SearchManufacturer'
 
-export { Button, Footer, Hero, Navbar }
+export { Button, Filter, Footer, Hero, Navbar, Searchbar, SearchManufacturer }
